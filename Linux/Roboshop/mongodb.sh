a694354d40c1a3335ac06cdbf2459696bf07ea4f
pentadushyanth <pentadushyanth@gmail.com>
2026-04-08 19:49:40 +0530

    third linux commit

--- Linux/Roboshop/mongodb.sh
+++ Linux/Roboshop/mongodb.sh
@@ -5,9 +5,9 @@ logfile="/var/log/logfile.log"
 Validate() {
 if [ $1 -eq 0 ]
 then 
-    echo "success"
+    echo " $2 is success"
 else
-    echo "failed" 
+    echo "$2 is failed" 
     exit 1
 fi
 }
@@ -15,12 +15,14 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo >> $logfile
 Validate $?
 
 dnf install mongodb-org -y >> $logfile
-Validate $?
+Validate $? "mongodb installation"
 systemctl enable mongod 
-Validate $?
+Validate $? "enabled mongodb"
 systemctl start mongod 
-Validate $?
-sed 's/127.0.0.1 /0.0.0.0 /g' /etc/mongod.conf
-Validate $?
+Validate $? "started mongodb"
+
+sed -i 's/127.0.0.1 /0.0.0.0 /g' /etc/mongod.conf
+Validate $? "IP update"
 
 systemctl restart mongod
+Validate $? "restart"
