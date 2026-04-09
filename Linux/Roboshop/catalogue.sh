dbde4e597aef875fa49a5e4e57ac592476f860db
pentadushyanth <pentadushyanth@gmail.com>
2026-04-09 18:59:06 +0530

    fourth linux commit

---
+++ Linux/Roboshop/catalogue.sh
@@ -0,0 +1,44 @@
+#! /bin/bash  #Ensures the script runs with the correct shell
+
+mkdir -p /var/log
+logfile="/var/log/logfile.log"
+Validate() {
+if [ $1 -eq 0 ];
+then 
+    echo "$2 is success"
+else
+    echo "$2 is failed" 
+    exit 1
+fi
+}
+
+dnf module disable nodejs -y >>$logfile
+Validate $? "nodejs disable"
+
+dnf module enable nodejs:20 -y >>$logfile
+Validate $? "nodejs enable"
+
+dnf install nodejs -y >>$logfile
+Validate $? "nodejs installation"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop >>$logfile
+Validate $? "user added"
+
+mkdir /app 
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+cd /app 
+unzip /tmp/catalogue.zip >>$logfile
+Validate $? "unzip"
+
+npm install >>$logfile
+Validate $? "dependencies addition"
+
+cp /catalogue.service  /etc/systemd/system >>$logfile
+
+systemctl daemon-reload
+
+systemctl enable catalogue 
+Validate $? "catalogue enable"
+systemctl start catalogue >>$logfile
+Validate $? "catalogue start"
+
