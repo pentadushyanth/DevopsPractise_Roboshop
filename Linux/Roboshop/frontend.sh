f7697101a75e654652213169f27c3889ad3e502e
pentadushyanth <pentadushyanth@gmail.com>
2026-04-15 19:51:14 +0530

    seventh linux commit

--- Linux/Roboshop/frontend.sh
+++ Linux/Roboshop/frontend.sh
@@ -20,6 +20,10 @@ Validate $? "nginx disable"
 dnf module enable nginx:1.24 -y >>$logfile
 Validate $? "nginx enable"
 
+rm -rf /etc/nginx/nginx.conf/* &>>$logfile
+VALIDATE $? "removed nginx content"
+
+
 dnf install nginx -y >>$logfile
 Validate $? "nginx installation"
 
